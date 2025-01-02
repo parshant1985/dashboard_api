4d81946bbb5ce4f70e21df322246b242d4d30d93
Nishaharpreet <harpreetsood90@gmail.com>
2025-01-02 11:40:06 +0530

    add model

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const User =  require('./models/user.model');
 require('dotenv').config();
 require('./db');
 const PORT = process.env.PORT || 8080;
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -16,6 +18,15 @@ app.get('/ping', (req, res) => {
 app.get('/country', (req, res) => {
     res.send('<=INDIA=>');
 });
+app.get('/api/users', async (req, res)=> {
+    try {
+     const users =  await User.find({})
+     res.status(200).json(users)
+    }
+    catch(error) {
+        res.status(500).json({error: error.message})
+    }
+});
 
 app.get('/ping', (req, res) => {
     res.send('PONG')
@@ -24,7 +35,15 @@ app.get('/ping', (req, res) => {
 app.use('/products', productRoutes);
 // /users
 app.use('/users', userRoutes);
-
-app.listen(8080, () => {
+app.get('/api/users', async (req, res)=> {
+    try {
+     const users =  await User.find({})
+     res.status(200).json(users)
+    }
+    catch(error) {
+        res.status(500).json({error: error.message})
+    }
+});
+app.listen(3000, () => {
     console.log('Server is listenin on PORT :' + PORT);
 })
